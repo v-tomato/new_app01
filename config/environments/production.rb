32f9a887b0bb9b8568bb574062283d4fc8159d30
EC2 Default User <ec2-user@ip-172-31-33-74.ap-northeast-1.compute.internal>
2020-05-04 08:38:16 +0000

    heroku-アプリ名変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # 本番環境の設定・メイラー
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'powerful-beyond-12488.herokuapp.com'
+  host = 'floating-brushlands-59840.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
