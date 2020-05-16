fe9cff3385f74676032cfdf218cbb701cc1687fd
EC2 Default User <ec2-user@ip-172-31-33-74.ap-northeast-1.compute.internal>
2020-05-16 13:10:57 +0000

    修正-privare-micropost-params

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -41,7 +41,7 @@ class MicropostsController < ApplicationController
 
   private
     def micropost_params
-      params.require(:micropost).permit(:memo, :time, )
+      params.require(:micropost).permit(:memo, :time )
     end
 
     def correct_user
