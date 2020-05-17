9452adbea8b5d8338342b2b449763ccc5b72a0a7
EC2 Default User <ec2-user@ip-172-31-33-74.ap-northeast-1.compute.internal>
2020-05-17 13:47:03 +0000

    shuusei

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -15,7 +15,7 @@ class MicropostsController < ApplicationController
   end
 
   def edit
-    @micropost = current_user.microposts.find_by(id: params[:id]) || nil
+    @microposts = current_user.microposts.find_by(id: params[:id]) || nil
     if @micropost.nil?
       flash[:warning] = "編集権限がありません"
       redirect_to root_url
